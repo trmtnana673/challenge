ba72e2dad76c5b0a26b429e17d915775c08ac1c6
DESKTOP-JAM4SIU\nana <trmtnana673@gmail.com>
2017-04-14 19:40:16 +0900

    Javaオブジェクト指向1「抽象化2」課題提出します

---
+++ CampChallenge_java/005.オブジェクト指向１/challenge5-2/Object2.java
@@ -0,0 +1,100 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.mypackage.ob;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ * @author nana
+ */
+public class Object2 extends HttpServlet {
+
+    /**
+     * Processes requests for both HTTP <code>GET</code> and <code>POST</code>
+     * methods.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    protected void processRequest(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        try (PrintWriter out = response.getWriter()) {
+            /* TODO output your page here. You may use following sample code. */
+            out.println("<!DOCTYPE html>");
+            out.println("<html>");
+            out.println("<head>");
+            out.println("<title>Servlet Object2</title>");            
+            out.println("</head>");
+            out.println("<body>");
+            
+            out.println("<h2>抽象化 「サッカー」</h2>");
+            out.println("<h3>プレイヤー</h3>");
+            out.println("役目：プレイヤー<br>情報：名前、年齢<br>動作：走る、蹴る<br>");
+            out.println("<h3>審判</h3>");
+            out.println("役目：審判<br>情報：名前、年齢<br>動作：走る、判定する<br>");
+            out.println("<h3>ボール</h3>");
+            out.println("役目：ボール<br>情報：メーカー、材質<br>動作：動く、停止する<br>");
+            
+            out.println("<h3>クラス</h3>");
+            out.println("<h4>人クラス</h4>");
+            out.println("・名前<br>・年齢<br>");
+            out.println("<h4>道具クラス</h4>");
+            out.println("・メーカー<br>・材質<br>");
+            
+            out.println("</body>");
+            out.println("</html>");
+        }
+    }
+
+    // <editor-fold defaultstate="collapsed" desc="HttpServlet methods. Click on the + sign on the left to edit the code.">
+    /**
+     * Handles the HTTP <code>GET</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Handles the HTTP <code>POST</code> method.
+     *
+     * @param request servlet request
+     * @param response servlet response
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException if an I/O error occurs
+     */
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        processRequest(request, response);
+    }
+
+    /**
+     * Returns a short description of the servlet.
+     *
+     * @return a String containing servlet description
+     */
+    @Override
+    public String getServletInfo() {
+        return "Short description";
+    }// </editor-fold>
+
+}
